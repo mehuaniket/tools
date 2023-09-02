695494880de1581c5b5949c710c78431e886dbcc
Aniket Patel <8078990+mehuaniket@users.noreply.github.com>
2023-09-03 00:55:41 +0530

    Update to v0.3.0 wooshh.rb

--- Formula/wooshh.rb
+++ Formula/wooshh.rb
@@ -1,9 +1,9 @@
 class Wooshh < Formula
     desc "A tool to play sound when cmd run successfully"
     homepage "https://github.com/mehuaniket/woshh"
-    url "https://github.com/mehuaniket/wooshh/releases/download/v0.2.0/wooshh-v0.2.0-x86_64-apple-darwin.tar.gz"
-    sha256 "c675b4783a56a95a6f5810b147d717ea99533573c05545f01ca40fea0ef39569"
-    version "0.2.0"
+    url "https://github.com/mehuaniket/wooshh/releases/download/v0.3.0/wooshh-v0.3.0-x86_64-apple-darwin.tar.gz"
+    sha256 "759eea85cb039b673cd96f08f1a356b6e28f22960d7565a765095e639f6ee769"
+    version "0.3.0"
   
     def install
       bin.install "wooshh"
