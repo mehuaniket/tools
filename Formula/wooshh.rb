ad6f8db1ae382fb7ace936d89b0d732e91c042f0
Aniket Patel <8078990+mehuaniket@users.noreply.github.com>
2026-04-10 16:56:47 +0530

    updating installation process for woosh

--- Formula/wooshh.rb
+++ Formula/wooshh.rb
@@ -1,11 +1,38 @@
 class Wooshh < Formula
-    desc "A tool to play sound when cmd run successfully"
-    homepage "https://github.com/mehuaniket/woshh"
-    url "https://github.com/mehuaniket/wooshh/releases/download/v0.2.1/wooshh-v0.2.1-x86_64-apple-darwin.tar.gz"
-    sha256 "859732114c5e83b8cc921af4aed1b5958774502a32d6bbeaadfb7fc102475fc3"
-    version "0.2.1"
-  
-    def install
-      bin.install "wooshh"
+  desc "A tool to play sound when cmd run successfully"
+  homepage "https://github.com/mehuaniket/wooshh"
+  version "0.3.0"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/mehuaniket/wooshh/releases/download/v#{version}/wooshh-bundle-v#{version}-aarch64-apple-darwin.tar.gz"
+      sha256 "d9b075fdb1c23d7230dd9362a47b8990d748228c09193a447f63479f65951103"
+    end
+
+    on_intel do
+      url "https://github.com/mehuaniket/wooshh/releases/download/v#{version}/wooshh-bundle-v#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "5dbf6f1cfd55e5a3ddf94e36d40791c3a9ec8977c698420006d630e7e0a65852"
     end
   end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/mehuaniket/wooshh/releases/download/v#{version}/wooshh-v#{version}-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "38e8d3be2cfccf20b42cd1d94b11378fd7bf896fdbcb3225a60656659539cb42"
+    end
+  end
+
+  def install
+    bin.install Dir["**/wooshh"].first
+    libexec.install Dir["**/WooshhNotifier.app"].first if OS.mac?
+  end
+
+  def post_install
+    return unless OS.mac?
+    return unless (libexec/"WooshhNotifier.app").exist?
+
+    system "/bin/mkdir", "-p", "/Applications"
+    system "/bin/rm", "-rf", "/Applications/WooshhNotifier.app"
+    system "/bin/cp", "-R", (libexec/"WooshhNotifier.app"), "/Applications/"
+  end
+end
